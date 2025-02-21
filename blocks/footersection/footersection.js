25f0314932840da9a71f562f6c4882fc1ea4c83a
Maksym Viter <maksym.viter@netcentric.biz>
2025-02-21 11:13:30 +0100

    lint fix

--- blocks/footersection/footersection.js
+++ blocks/footersection/footersection.js
@@ -5,15 +5,13 @@ export default function decorate(block) {
 
   links.forEach((link) => {
     if (link.classList.length > 0) {
-      link.classList.value = ''; // Clear all classes
+      link.classList.value = '';
     }
 
-    // Find the closest <picture> element to the <a> tag
     const picture = link.closest('li')?.querySelector('picture');
 
     if (picture) {
-      // Move the <picture> element inside the <a> tag
-      link.innerHTML = ''; // Remove any existing text or elements inside the <a> tag
+      link.innerHTML = '';
       link.appendChild(picture);
     }
   });
